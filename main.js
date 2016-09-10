9542f1a886120864798a1210763f25c8c619492f
Angela Adams <aadams3867@gmail.com>
2016-09-10 01:38:47 -0400

    Fixes incorrect filename

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ var inquirer = require('inquirer');
 // Required files to run this game
 var game = require('./game.js');  			// Contains the method that randomly selects a word for the player to guess
 var Letter = require('./letter.js');    	// Contains the method that controls whether or not a letter appears as a "___" or as itself on-screen
-//var Word = require('./word.js');  		// Contains all of the methods that check the letters guessed vs the random word selected
+var Word = require('./word.js');  			// Contains all of the methods that check the letters guessed vs the random word selected
 
 // Initialize global variables
 var wordToGuess = game.randomWord;  						// Random word selected
-var letterGuess = "";										// Current letter the player guessed
-var displayArray = new Letter(wordToGuess, letterGuess);	// Stores the player's current display of letters and "___"
+var letterGuess = "R";										// Current letter the player guessed
+var displayArray = new Word(wordToGuess, letterGuess);	// Stores the player's current display of letters and "___"
 //var lettersGuessed = 0;										// Counts the number of correct letter guesses
 var letterArray = [];  										// Stores all the letters guessed by the player so far
 var guessesLeft = 7;  										// 7 wrong letter guesses before the game ends
--- word.js
+++ word.js
@@ -1,10 +1,21 @@
 // CONSTRUCTOR FILE
 // Contains all of the methods that check the letters guessed vs the random word selected
 
-var Student = function(studentName, faveSubject, gpa){
-	this.name = studentName;
-	this.subject = faveSubject;
-	this.gpa = gpa;
+var Word = function(word, letter){
+	// Convert the random word into an array of characters
+	var wordArray = word.split("");
+
+	// Checks whether the player's letter pick appears in the random word
+	for (var i=0; i<wordArray.length; i++) {
+		if (wordArray[i] === letter) {
+			wordArray[i] =  " " + letter + " ";
+		} else {
+			wordArray[i] = "___";
+		}
+	}
+
+	// Convert the array of characters back into a string
+	this.display = wordArray.join(" ");
 };
 
 module.exports = Word;
